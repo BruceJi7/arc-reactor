b04785a11cf168ee418ac0388d1a4019a5e76931
bruceji7 <revolverc24@gmail.com>
2021-11-01 14:34:57 +0900

    This code positions X things on an arc

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,75 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+  function degToRadian(deg:number){ 
+    return deg * (Math.PI/180);
+  }
+
+  function getXcoord(deg:number, hypo:number){
+
+    // The sin of the degree is equal to ANS divided by HYPO
+    // Get sin of degree.
+    // Multiply this number by the HYPO
+    // This will be the length co-ord
+
+    const radA = degToRadian(deg)
+    const sinA = Math.sin(radA)
+    const xLength = sinA * hypo
+    
+    return xLength
+  }
+
+  function getYcoord(deg:number, hypo:number){
+
+    // The sin of the degree is equal to ANS divided by HYPO
+    // Get sin of degree.
+    // Multiply this number by the HYPO
+    // This will be the length co-ord
+
+    const radA = degToRadian(deg)
+    const cosA = Math.cos(radA)
+    const yLength = cosA * hypo
+    
+    return yLength
+  }
+
+  function getTrigoXY(deg:number, height:number, width:number){
+    const y = getYcoord(deg, height)
+    const x = getXcoord(deg, width)
+    return {x, y}
+  }
+
+  function makeImages(howMany:number, correction:number=0){
+
+    const circles = []
+
+    for (let i=0; i<=howMany-1; i++){
+
+      const angle = (90 / howMany+1) * i - ((90/howMany)*(howMany/2) - correction)
+      const position = getTrigoXY(angle, -200, 700);          
+      circles.push(<img src={logo} className="atom" alt="logo" style={{top:position.y, left:position.x}} />) 
+    }
+
+    return circles
+
+  }
+
+
+  const circles = makeImages(7);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="atomBox">
+
+      {
+        circles.map((el)=> el)
+      }
+      
+      </div>
     </div>
   );
 }
